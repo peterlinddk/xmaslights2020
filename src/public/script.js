8a8b1605f58aaf18426b8524f32dffb57ff3f55d
Peter Lind <github@peterlind.dk>
2020-11-24 23:56:37 +0100

    added complete object model for timelines and spans - started timespan editor

--- src/public/script.js
+++ src/public/script.js
@@ -15,7 +15,13 @@ async function loadSequence() {
   const resp = await fetch("sequence.json");
   const data = await resp.json();
 
-  return data;
+  // create new objects for everything
+  const objseq = new Sequence();
+  data.tracks.forEach((track) => {
+    objseq.addTrack(track);
+  });
+
+  return objseq;
 }
 
 function buildSequence() {
@@ -35,32 +41,35 @@ function buildSequence() {
       <span data-start-time="5:30" data-end-time="5:45" data-value="on"></span>
     </div>
   */
-  sequence.tracks.forEach((track, index) => {
+  sequence.tracks.forEach((track) => {
     const infodiv = document.createElement("div");
-    infodiv.id = `track${index}_id`;
+    infodiv.id = `track${track.index}_id`;
     infodiv.textContent = track.name;
 
     tracks.insertBefore(infodiv, controls);
 
     const timelinediv = document.createElement("div");
-    timelinediv.id = `track${index}_timeline`;
+    timelinediv.id = `track${track.index}_timeline`;
     timelinediv.classList.add("timeline");
+    timelinediv.dataset.uuid = track.timeline.uuid;
+
+    track.timeline.element = timelinediv;
 
     // create timeline and spans
-    track.timeline.forEach(time => {
+    track.timeline.timespans.forEach((timeSpan) => {
       const span = document.createElement("span");
-      span.dataset.startTime = time.start;
-      span.dataset.endTime = time.end;
+      span.dataset.startTime = timeSpan.start.timecode;
+      span.dataset.endTime = timeSpan.end.timecode;
       span.dataset.value = "on";
+      span.dataset.uuid = timeSpan.uuid;
+
+      timeSpan.element = span;
 
       timelinediv.append(span);
-
     });
     tracks.insertBefore(timelinediv, controls);
   });
- 
 }
-
 
 function loaded() {
   console.log("Loaded");
@@ -69,7 +78,11 @@ function loaded() {
   positionSpans();
   window.addEventListener("resize", resize);
 
-  document.querySelectorAll("[data-action]").forEach(button => button.addEventListener("click", performAction));
+  // add editor-feature to spans
+  document.querySelectorAll(".timeline span").forEach((span) => span.addEventListener("mousedown", timespanEditor));
+
+  // add other button actions (none implemented at the moment)
+  document.querySelectorAll("[data-action]").forEach((button) => button.addEventListener("click", performAction));
 }
 
 function resize() {
@@ -90,11 +103,11 @@ function buildSVG() {
 
   const width = svg.clientWidth;
   // one hour is width/24 - one minute is width/1440
-  const hourWidth = width / 24 * zoomFactor;
+  const hourWidth = (width / 24) * zoomFactor;
   const minuteWidth = hourWidth / 60;
 
   // set viewBox
-  svg.setAttribute("viewBox", `${width*scroller.getOffset()*zoomFactor} 0 ${width} 20`);
+  svg.setAttribute("viewBox", `${width * scroller.getOffset() * zoomFactor} 0 ${width} 20`);
 
   const ruler = svg.querySelector("#ruler");
   // remove everything inside the ruler
@@ -109,9 +122,9 @@ function buildSVG() {
     if (h > 0 && h < 24) {
       const text = document.createElementNS("http://www.w3.org/2000/svg", "text");
       text.classList.add("small");
-      text.setAttribute("x", h*hourWidth); // TODO: Find correct pixel-width
+      text.setAttribute("x", h * hourWidth); // TODO: Find correct pixel-width
       text.setAttribute("y", 20);
-      text.textContent = `${h.toString().padStart(2," ")}:00`;
+      text.textContent = `${h.toString().padStart(2, " ")}:00`;
       ruler.append(text);
     }
 
@@ -120,7 +133,7 @@ function buildSVG() {
     // add quarter-line 1
     addLine(h * hourWidth + hourWidth / 4, 3);
     // add quarter-line 2
-    addLine(h*hourWidth+hourWidth/4+hourWidth/2,3);
+    addLine(h * hourWidth + hourWidth / 4 + hourWidth / 2, 3);
   }
 
   function addLine(x, height) {
@@ -135,32 +148,10 @@ function buildSVG() {
 
 function positionSpans() {
   // find all spans in all timelines, and set their left and width depending on start and end-time
-  document.querySelectorAll(".timeline").forEach(timeline => {
-  
-    // Find width of this timeline - should be the same for all of them
-    const width = timeline.clientWidth * zoomFactor;
-    // one hour is width/24 - one minute is width/1440
-    const hourWidth = width / 24;
-    const minuteWidth = hourWidth / 60;
-
-    const offset = width * scroller.getOffset();
-    
-    timeline.querySelectorAll("span").forEach(span => {
-      const startTime = span.dataset.startTime;
-      const endTime = span.dataset.endTime;
-
-      let [hour, minute] = startTime.split(":").map(val => Number(val));
-      
-      const pixelStart = hour * hourWidth + minute * minuteWidth;
-      span.style.left = pixelStart - offset + "px";
-
-      [hour, minute] = endTime.split(":").map(val => Number(val));
-
-      const pixelWidth = hour * hourWidth + minute * minuteWidth - pixelStart;
-      span.style.width = pixelWidth + "px";
-    });
+  sequence.tracks.forEach((track) => {
+    const timeline = track.timeline;
+    timeline.timespans.forEach((timeSpan) => timeSpan.position());
   });
-    
 }
 
 function performAction(event) {
@@ -196,12 +187,11 @@ function setZoom(zoom) {
 /* scroller */
 
 const scroller = {
-
   init() {
     this.scrollbar = document.querySelector("#scrollbar");
     this.scrollarea = document.querySelector("#scroller");
 
-    this.scrollWidth = this.scrollarea.clientWidth 
+    this.scrollWidth = this.scrollarea.clientWidth;
     this.scrollBarX = 0;
     this.scrollBarW = this.scrollWidth;
 
@@ -277,7 +267,7 @@ const scroller = {
     this.scrollarea.removeEventListener("mousemove", this);
     this.scrollarea.removeEventListener("mouseup", this);
     this.scrollarea.removeEventListener("mouseleave", this);
-    
+
     this.grabbedElement = null;
   },
   move(event) {
@@ -296,7 +286,7 @@ const scroller = {
 
     if (this.grabbedElement === "scrollbar") {
       // move entire scrollbar to the dragged position (with the controlPoint!)
-      let newX = this.dragX - this.controlPoint; 
+      let newX = this.dragX - this.controlPoint;
 
       // Prevent moving to far to the left
       if (newX < 0) {
@@ -304,17 +294,17 @@ const scroller = {
       }
 
       // Prevent moving to far to the right
-      if (newX > maxX  ) {
+      if (newX > maxX) {
         newX = maxX;
       }
-        
+
       this.scrollBarX = newX;
     } else if (this.grabbedElement === "end") {
       // move end by setting the width to match the new dragX position
       let newW = this.controlPoint + this.remainder + (this.dragX - this.startX);
-      
+
       // Prevent moving to far to the right
-      if (newW > maxWidth ) {
+      if (newW > maxWidth) {
         newW = maxWidth;
       }
       // Prevent moving to far to the left
@@ -325,7 +315,7 @@ const scroller = {
       this.scrollBarW = newW;
     } else if (this.grabbedElement === "start") {
       // move start first by setting the newX to the dragged position
-      let newX = this.dragX - this.controlPoint; 
+      let newX = this.dragX - this.controlPoint;
 
       if (newX < 0) {
         newX = 0;
@@ -341,13 +331,13 @@ const scroller = {
         }
         // Prevent moving to far to the right
         if (newW < minWidth) {
-          // Do NOT modify scrollbarX or W if the scrollbar has been moved to be to small 
+          // Do NOT modify scrollbarX or W if the scrollbar has been moved to be to small
           newW = this.scrollBarW;
           newX = this.scrollBarX;
         }
         this.scrollBarW = newW;
-      }      
-      
+      }
+
       this.scrollBarX = newX;
     }
 
@@ -358,6 +348,221 @@ const scroller = {
   updateScrollBar() {
     this.scrollbar.style.left = this.scrollBarX + "px";
     this.scrollbar.style.width = this.scrollBarW + "px";
+  },
+};
+
+function getTimeSpanFromUuid(uuid) {
+  let timeSpan = null;
+  for (let i = 0; i < sequence.tracks.length; i++) {
+    timeSpan = sequence.tracks[i].timeline.timespans.find((span) => span.uuid === uuid);
+    if (timeSpan) {
+      break;
+    }
+  }
+  return timeSpan;
+}
+
+/* timeline edit */
+const timespanEditor = {
+  select(event) {
+    const element = event.currentTarget;
+
+    // find the selected timespan
+    this.span = getTimeSpanFromUuid(element.dataset.uuid);
+
+    // mark the span as selected
+    this.span.element.classList.add("selected");
+
+    // Add eventlisteners to the timeline (so we can move outside this span)
+    this.span.timeline.element.addEventListener("mousemove", this);
+    this.span.timeline.element.addEventListener("mouseup", this);
+    this.span.timeline.element.addEventListener("mouseleave", this);
+
+    // Figure out if it was selected for moving, expanding left or right
+    // console.log(event);
+    const controlPoint = event.layerX;
+
+    if (controlPoint < 16) {
+      // hardcoded value to grab the beginning
+      this.selectionType = "left";
+      this.span.element.classList.add("resize");
+    } else if (controlPoint > this.span.element.clientWidth - 16) {
+      this.selectionType = "right";
+      this.span.element.classList.add("resize");
+    } else {
+      this.selectionType = "move";
+      this.span.element.classList.add("move");
+    }
+
+    this.startX = event.clientX;
+
+    this.initialStart = new TimeCode(this.span.start);
+    this.initialEnd = new TimeCode(this.span.end);
+
+    console.log(`Selected span `);
+    console.log(this.span);
+  },
+  handleEvent(event) {
+    // console.log(`Event type: ${event.type}`);
+    if (event.type === "mouseup" || event.type === "mouseleave") {
+      this.release(event);
+    }
+    if (event.type === "mousedown") {
+      this.select(event);
+    }
+    if (event.type === "mousemove") {
+      this.move(event);
+    }
+  },
+  release(event) {
+    console.log("Release");
+    this.span.element.classList.remove("selected");
+    this.span.element.classList.remove("resize");
+    this.span.element.classList.remove("move");
+    this.span.timeline.element.removeEventListener("mousemove", this);
+    this.span.timeline.element.removeEventListener("mouseup", this);
+    this.span.timeline.element.removeEventListener("mouseleave", this);
+
+    // forget about the selected element
+    this.span = null;
+  },
+  move(event) {
+    console.log("move");
+
+    // find relative move-distance
+    const moveX = event.clientX;
+    const distance = moveX - this.startX;
+    // console.log(`Moved ${distance}`);
+
+    // convert moved distance into minutes
+    const minutes = distance / this.span.timeline.minuteWidth;
+    // console.log(`Minutes ${minutes}`);
+
+    // TODO: Handle all different selectionTypes - not only start
+
+    // new Time has to be created from the initial start time on every edit, to avoid accumulating changes
+    const newTime = new TimeCode(this.initialStart);
+    newTime.addMinutes(minutes);
+
+    this.span.start.timecode = newTime.timecode;
+    this.span.position();
+  },
+};
+
+
+class Sequence {
+  constructor() {
+    this.tracks = [];
   }
 
+  addTrack(track) {
+    const objtrack = new Track(track);
+    objtrack.index = this.tracks.length;
+    this.tracks.push(objtrack);
+  }
+}
+
+class Track {
+  constructor(track) {
+    this.name = track.name;
+    this.timeline = new TimeLine(track.timeline);
+  }
+}
+
+/* The timeline is the entire timeline for a track - connected to the visual timeline, and with all the timespans as children */
+class TimeLine {
+  constructor(timeline) {
+    this.timespans = [];
+
+    // The timeline argument is only an array of timespans
+    this.timespans = timeline.map((span) => new TimeSpan(span, this));
+
+    this.uuid = uuidv4();
+  }
+
+  get width() {
+    return this.element.clientWidth * zoomFactor;
+  }
+
+  get offset() {
+    return this.width * scroller.getOffset();
+  }
+
+  get hourWidth() {
+    return this.width / 24;
+  }
+
+  get minuteWidth() {
+    return this.hourWidth / 60;
+  }
+}
+
+class TimeSpan {
+  constructor(span, timeline) {
+    this.timeline = timeline; // know the parent timeline
+    this.start = new TimeCode(span.start);
+    this.end = new TimeCode(span.end);
+    this.uuid = uuidv4();
+  }
+
+  position() {
+    const pixelStart = this.start.hour * this.timeline.hourWidth + this.start.minute * this.timeline.minuteWidth;
+    this.element.style.left = pixelStart - this.timeline.offset + "px";
+    const pixelWidth = this.end.hour * this.timeline.hourWidth + this.end.minute * this.timeline.minuteWidth - pixelStart;
+    this.element.style.width = pixelWidth + "px";
+  }
+}
+
+// Unique ID - from: https://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid
+function uuidv4() {
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+    var r = (Math.random() * 16) | 0,
+      v = c == "x" ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+}
+
+class TimeCode {
+  constructor(str) {
+    // if argument is another TimeCode-object, call clone instead
+    if (str instanceof TimeCode) {
+      this.clone(str);
+    } else {
+      this.timecode = str;
+    }
+  }
+
+  clone(obj) {
+    this.hour = obj.hour;
+    this.minute = obj.minute;
+  }
+
+  toString() {
+    return this.timecode;
+  }
+
+  get timecode() {
+    return `${this.hour}:${this.minute.toString().padStart(2, "0")}`;
+  }
+
+  set timecode(str) {
+    const [hour, minute] = str.split(":").map((val) => Number(val));
+    this.hour = hour;
+    this.minute = minute;
+  }
+
+  get decimalTime() {
+    return this.hour + this.minute / 60;
+  }
+
+  set decimalTime(dtime) {
+    const hour = Math.floor(dtime);
+    const minute = Math.round((dtime - hour) * 60);
+    this.hour = hour;
+    this.minute = minute;
+  }
+
+  addMinutes(minutes) {
+    this.decimalTime = this.decimalTime + minutes / 60;
+  }
 }
